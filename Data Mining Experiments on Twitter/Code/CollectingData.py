c846935e88e3fea1306a04ee9c36d247c49dd59f
Himanshu <hphimanshu6@gmail.com>
2018-09-14 12:45:01 -0400

    removing the twitter keys

--- Data Mining Experiments on Twitter/Code/CollectingData.py
+++ Data Mining Experiments on Twitter/Code/CollectingData.py
@@ -1,10 +1,10 @@
 import tweepy
 from tweepy import OAuthHandler
 
-consumer_key = 'Umayc6h26nLIstty28Y7nF4en'
-consumer_secret = 'k9trHAqNaV4F3bLAoh0rO5wCDcsiypih0wcXdk08jjekt4mTh7'
-access_token = '793873801788346368-sMGXwGzi6LMTAd5QMNXvd9hjmCKuG6o'
-access_secret = 'QTd3rYvM0e558K1usiBVUjz7YtwUq8fMYJZjiL2AhXSYs'
+consumer_key = 
+consumer_secret = 
+access_token = 
+access_secret = 
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
